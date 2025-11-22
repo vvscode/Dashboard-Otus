42dacf12aff9899547399509d88e88481d0d6978
Илья Петров <brahmagupta@tuta.io Primary Verified>
2025-11-22 07:27:34 +0300

    build: настроил webpack. Теперь при деплое на gitHubPages не должно быть проблем с 404 ошибкой

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const PREFIX = "название url после деплоя на GH Pages";
 
 export default {
   mode: "development",
@@ -13,9 +15,20 @@ export default {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
+    publicPath: process.env.NODE_ENV === "production" ? PREFIX : "/",
   },
 
-  plugins: [new HtmlWebpackPlugin({ template: "./src/dashBoard.html" })],
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/dashBoard.html" }),
+    new HtmlWebpackPlugin({
+      template: "./src/dashBoard.html",
+      filename: "404.html",
+    }),
+    new webpack.DefinePlugin({
+      PRODUCTION: JSON.stringify(process.env.NODE_ENV === "production"),
+      PREFIX: JSON.stringify(PREFIX),
+    }),
+  ],
 
   devServer: {
     static: {
@@ -26,6 +39,8 @@ export default {
 
     port: 9000,
 
+    historyApiFallback: true,
+
     client: {
       overlay: {
         errors: true,
@@ -34,3 +49,5 @@ export default {
     },
   },
 };
+
+//Такая настройка webpack именно для GH Pages нужна? С норм хостингом, где работает history api такой проблемы не будет?
